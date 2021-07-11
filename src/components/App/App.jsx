eb8ca395e29e2da759d97d5d2649b82c532faa8a
Ostap Pluzhnik <plostvik@gmail.com>
2021-07-11 22:06:56 +0300

    changed function structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -211,65 +211,7 @@ const App = () => {
   // }
 
   //*fourth refactoring
-
-  function amountFor(aPerfomance) {
-    let result = 0;
-    switch (playFor(aPerfomance).type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerfomance.audience > 30) {
-          result += 1000 * (aPerfomance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerfomance.audience > 20) {
-          result += 10000 + 500 * (aPerfomance.audience - 20);
-        }
-        result += 300 * aPerfomance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${playFor(aPerfomance).type}`);
-    }
-    return result;
-  }
-
-  function playFor(aPerfomance) {
-    return plays[aPerfomance.playID];
-  }
-
-  function volumeCreditsFor(aPerfomance) {
-    let result = 0;
-    result += Math.max(aPerfomance.audience - 30, 0);
-    if ('comedy' === playFor(aPerfomance).type) {
-      result += Math.floor(aPerfomance.audience / 5);
-    }
-    return result;
-  }
-
-  function totalvolumeCredits(invoice) {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += volumeCreditsFor(perf);
-    }
-    return result;
-  }
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(
-      aNumber,
-    );
-  }
-
-  function totalAmount(invoice) {
-    let result = 0;
-    for (let perf of invoice.performances) {
-      result += amountFor(perf);
-    }
-    return result;
-  }
-
-  function statement(invoice) {
+  function statement(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
@@ -281,9 +223,66 @@ const App = () => {
     result += `Amount owed is ${usd(totalAmount(invoice) / 100)}\n`;
     result += `You earned ${totalvolumeCredits(invoice)} credits\n`;
     return result;
+
+    function amountFor(aPerfomance) {
+      let result = 0;
+      switch (playFor(aPerfomance).type) {
+        case 'tragedy':
+          result = 40000;
+          if (aPerfomance.audience > 30) {
+            result += 1000 * (aPerfomance.audience - 30);
+          }
+          break;
+        case 'comedy':
+          result = 30000;
+          if (aPerfomance.audience > 20) {
+            result += 10000 + 500 * (aPerfomance.audience - 20);
+          }
+          result += 300 * aPerfomance.audience;
+          break;
+        default:
+          throw new Error(`unknown type: ${playFor(aPerfomance).type}`);
+      }
+      return result;
+    }
+
+    function playFor(aPerfomance) {
+      return plays[aPerfomance.playID];
+    }
+
+    function volumeCreditsFor(aPerfomance) {
+      let result = 0;
+      result += Math.max(aPerfomance.audience - 30, 0);
+      if ('comedy' === playFor(aPerfomance).type) {
+        result += Math.floor(aPerfomance.audience / 5);
+      }
+      return result;
+    }
+
+    function totalvolumeCredits() {
+      let result = 0;
+      for (let perf of invoice.performances) {
+        result += volumeCreditsFor(perf);
+      }
+      return result;
+    }
+
+    function usd(aNumber) {
+      return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(
+        aNumber,
+      );
+    }
+
+    function totalAmount() {
+      let result = 0;
+      for (let perf of invoice.performances) {
+        result += amountFor(perf);
+      }
+      return result;
+    }
   }
 
-  return <pre>{statement(invoices)}</pre>;
+  return <pre>{statement(invoices, plays)}</pre>;
 };
 
 export default App;
