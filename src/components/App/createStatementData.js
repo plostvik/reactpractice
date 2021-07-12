f8af93cbdfadd352eb1c57999b15ec4a1da55e60
Ostap Pluzhnik <opluz@softserveinc.com>
2021-07-12 10:37:00 +0300

    created class calculator for calculated logic

--- src/components/App/createStatementData.js
+++ src/components/App/createStatementData.js
@@ -1,52 +1,22 @@
 export default function createStatementData(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerfomance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalvolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
+  const result = {};
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerfomance);
+  result.totalAmount = totalAmount(result);
+  result.totalvolumeCredits = totalVolumeCredits(result);
+  return result;
 
   function enrichPerfomance(aPerfomance) {
+    const calculator = new PerfomanceCalculator(aPerfomance, playFor(aPerfomance));
     const result = Object.assign({}, aPerfomance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
   function playFor(aPerfomance) {
     return plays[aPerfomance.playID];
-  }
-
-  function amountFor(aPerfomance) {
-    let result = 0;
-    switch (aPerfomance.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerfomance.audience > 30) {
-          result += 1000 * (aPerfomance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerfomance.audience > 20) {
-          result += 10000 + 500 * (aPerfomance.audience - 20);
-        }
-        result += 300 * aPerfomance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerfomance.play.type}`);
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(aPerfomance) {
-    let result = 0;
-    result += Math.max(aPerfomance.audience - 30, 0);
-    if ('comedy' === aPerfomance.play.type) {
-      result += Math.floor(aPerfomance.audience / 5);
-    }
-    return result;
   }
 
   function totalVolumeCredits(data) {
@@ -55,5 +25,43 @@ export default function createStatementData(invoice, plays) {
 
   function totalAmount(data) {
     return data.performances.reduce((total, p) => total + p.amount, 0);
+  }
+}
+
+class PerfomanceCalculator {
+  constructor(aPerfomance, aPlay) {
+    this.perfomance = aPerfomance;
+    this.play = aPlay;
+  }
+
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case 'tragedy':
+        result = 40000;
+        if (this.perfomance.audience > 30) {
+          result += 1000 * (this.perfomance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (this.perfomance.audience > 20) {
+          result += 10000 + 500 * (this.perfomance.audience - 20);
+        }
+        result += 300 * this.perfomance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.perfomance.audience - 30, 0);
+    if ('comedy' === this.play.type) {
+      result += Math.floor(this.perfomance.audience / 5);
+    }
+    return result;
   }
 }
